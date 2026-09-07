622ff06b7481705633ce690b35cb1583d274e5d6
Willame Costa <lilly.menuu@gmail.com>
2026-09-07 17:23:56 -0300

    pequenos ajustes

--- public/planos.php
+++ public/planos.php
@@ -271,7 +271,7 @@ $planos = array_values(array_filter($planos, fn($p) => $p['ativo']));
       <div class="nav-links">
         <a href="index.php#inicio">Inicio</a>
         <a href="index.php#a-lilly">A <?= htmlspecialchars($brand) ?></a>
-        <a href="index.php#solucoes">Solucoes</a>
+        <a href="index.php#solucoes">Soluções</a>
         <a href="index.php#franquias">Franquias</a>
         <a href="planos">Planos</a>
       </div>
